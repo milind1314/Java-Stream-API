196989e73c65076e538150af8e5fe738e5bf60b5
Milind Atram <milindatram99@gmail.com>
2025-02-05 16:17:25 +0530

    Update CountVowel.java

--- Vowel count/CountVowel.java
+++ Vowel count/CountVowel.java
@@ -7,16 +7,21 @@ import java.util.stream.Collectors;
 
 public class CountVowel {
 
-	public static int checkVowel(String s)
-	{
-		return (int) s.chars().filter(c -> "aeiouAEIOU".indexOf(c) != -1).count();
-	}
-	public static void main(String[] args) {
+    // Method to count the number of vowels in a given string
+    public static int checkVowel(String s) {
+        return (int) s.chars().filter(c -> "aeiouAEIOU".indexOf(c) != -1).count();
+    }
 
-		List<String> strings = List.of("hello", "world", "java", "stream", "api");
-		
-		Map<String, Integer> collect = strings.stream().collect(Collectors.toMap(Function.identity(), CountVowel::checkVowel));
-		System.out.println(collect);
-	}
-
+    public static void main(String[] args) {
+        
+        // Creating a list of strings
+        List<String> strings = List.of("hello", "world", "java", "stream", "api");
+        
+        // Using streams to map each string to its vowel count
+        Map<String, Integer> collect = strings.stream()
+            .collect(Collectors.toMap(Function.identity(), CountVowel::checkVowel));
+        
+        // Printing the resulting map of string to vowel count
+        System.out.println(collect);
+    }
 }
